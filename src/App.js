ace1f035d39f80c3afbbc58e276b880dfa0795e3
ALLAN <allankipr6@gmail.com>
2024-05-02 18:02:41 +0300

    n

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React, { useState } from "react";
+
 function App() {
+  const [selectedInterests, setSelectedInterests] = useState([]);
+  const [formissubmited,setformissubmited]=useState(false)
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedInterests([...selectedInterests, value]);
+    } else {
+      setSelectedInterests(selectedInterests.filter((interest) => interest !== value));
+    }
+  };
+  function handlesubmit(e){
+    e.preventDefault()
+    setformissubmited(true)
+  }
+
   return (
-    <main>
+    <main >
       <h1>Hi, I'm (your name)</h1>
       <img alt="My profile pic" src="https://via.placeholder.com/350" />
       <h2>About Me</h2>
@@ -18,6 +35,38 @@ function App() {
         <a href="https://github.com">GitHub</a>
         <a href="https://linkedin.com">LinkedIn</a>
       </div>
+      
+      <form id="form">
+        <input type="text" placeholder="name" />
+        <input type="email" placeholder="email" />
+      </form>
+      <h2>Select your Interests</h2>
+      <form onSubmit={handlesubmit}>
+      <label>
+        <input type="checkbox" value="music" checked={selectedInterests.includes("music")} onChange={handleCheckboxChange} />
+        Music
+      </label>
+      <label>
+        <input type="checkbox" value="sports" checked={selectedInterests.includes("sports")} onChange={handleCheckboxChange} />
+        Sports
+      </label>
+      <label>
+        <input type="checkbox" value="cooking" checked={selectedInterests.includes("cooking")} onChange={handleCheckboxChange} />
+        Cooking
+      </label>
+      <label>
+        <input type="checkbox" value="driving" checked={selectedInterests.includes("driving")} onChange={handleCheckboxChange}/>
+        Driving
+      </label>
+      <h3>Selected Interests</h3>
+      <ul>
+        {selectedInterests.map((interest, index) => (
+          <li key={index}>{interest}</li>
+        ))}
+      </ul>
+      <button type="submit" >submit</button>
+      {formissubmited? <p>DONE!</p> : null}
+      </form>
     </main>
   );
 }
